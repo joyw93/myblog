5b3ff12d54f51e584d1c4720addf65cd58099af6
unknown <joyw93@naver.com>
2022-01-05 14:36:58 +0900

    html 상속

--- blog/tests.py
+++ blog/tests.py
@@ -8,9 +8,6 @@ class TestView(TestCase):
         self.client = Client()
 
     def test_post_list(self):
-        
-        
-
         # 1.1 포스트 목록 페이지(post list)를 연다.
         response = self.client.get('/blog/')
 
@@ -51,3 +48,33 @@ class TestView(TestCase):
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
+
+    def test_post_detail(self):
+        # 1.1 포스트가 하나 있다.
+        post_001 = Post.objects.create(
+            title = '',
+            content = '',
+        )
+        self.assertEqual(Post.objects.count(), 1)
+        # 1.2. 그 포스트의 url은 '/blog/1/'이다.
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # 2. 첫 번째 포스트의 상세 페이지 테스트
+        # 2.1. 첫 번째 포스트의 url로 접근하면 정상적으로 response가 온다
+        response = self.client.get(post_001.get_absoloute_url())
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # 2.2. 포스트 목록 페이지와 똑같은 네비게이션 바가 있다.
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다.
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.content, post_area.text)
+
+
