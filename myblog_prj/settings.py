cc7e18f00b0877495a82d04d452fd16166595d6e
unknown <joyw93@naver.com>
2022-01-23 04:07:05 +0900

    소셜로그인

--- myblog_prj/settings.py
+++ myblog_prj/settings.py
@@ -30,6 +30,12 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'crispy_forms',
+    'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     'blog',
     'single_pages',
@@ -94,6 +100,20 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTHENTICATION_BACKENDS = (
+    
+    'django.contrib.auth.backends.ModelBackend',
+    
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
 
 
 # Internationalization
--- myblog_prj/urls.py
+++ myblog_prj/urls.py
@@ -7,7 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('', include('single_pages.urls'),)
+    path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('single_pages.urls')),
 ]
 
 
